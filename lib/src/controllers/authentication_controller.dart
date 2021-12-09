4a45a6cbde95e42dc554e07b55f747f68c9dca97
Lý Ngọc Hữu <huuln9@gmail.com>
2021-12-09 09:42:59 +0700

    update

---
+++ lib/src/controllers/authentication_controller.dart
@@ -0,0 +1,44 @@
+import 'package:get/get.dart';
+import 'package:vncitizens_authentication/src/services/authentication_service.dart';
+
+class AuthenticationController {
+  AuthenticationService? _authenticationService;
+  AuthenticationStatus _status = AuthenticationStatus.unauthenticated;
+  String _accessToken = '';
+
+  AuthenticationController({
+    required String ssoURL,
+    required String clientId,
+    required String username,
+    required String password,
+  }) {
+    _authenticationService = Get.put(AuthenticationService(
+      ssoURL: ssoURL,
+      clientId: clientId,
+      username: username,
+      password: password,
+    ));
+  }
+
+  getAuthenticationStatus() => _status;
+  getAccessToken() => _accessToken;
+
+  signInWithCredential() async {
+    String accessToken = await _authenticationService!.signInWithCredential();
+    _accessToken = accessToken;
+  }
+
+  signInWithPassword({required username, required password}) async {
+    String accessToken = await _authenticationService!.signInWithPassword(
+      username: username,
+      password: password,
+    );
+    _status = AuthenticationStatus.authenticated;
+    _accessToken = accessToken;
+  }
+
+  signOut() {
+    _status = AuthenticationStatus.unauthenticated;
+    _accessToken = '';
+  }
+}
---
+++ lib/src/services/authentication_service.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+enum AuthenticationStatus { unauthenticated, authenticated }
+
+class AuthenticationService extends GetConnect {
+  String ssoURL;
+  String clientId;
+  String username;
+  String password;
+
+  AuthenticationService({
+    required this.ssoURL,
+    required this.clientId,
+    required this.username,
+    required this.password,
+  });
+
+  Future<String> signInWithCredential() async {
+    final String accessToken = await _getAccessTokenWithPassword(
+        username: username, password: password);
+    return accessToken;
+  }
+
+  Future<String> signInWithPassword({
+    required String username,
+    required String password,
+  }) async {
+    final String accessToken = await _getAccessTokenWithPassword(
+        username: username, password: password);
+
+    return accessToken;
+  }
+
+  Future<String> _getAccessTokenWithPassword({
+    required String username,
+    required String password,
+  }) async {
+    Map<String, dynamic> requestBody = {
+      'grant_type': 'password',
+      'client_id': clientId,
+      'username': username,
+      'password': password,
+    };
+
+    final response = await http.post(
+      Uri.parse('$ssoURL/auth/realms/digo/protocol/openid-connect/token'),
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: requestBody,
+    );
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body)['access_token'];
+    } else {
+      throw Exception(response.body);
+    }
+  }
+}
