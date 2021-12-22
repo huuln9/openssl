3ae44852f51bcac0119aec05adbe696e033de9c5
Lý Ngọc Hữu <huuln9@gmail.com>
2021-12-22 09:56:30 +0700

    update ui login

--- lib/vncitizens_authentication.dart
+++ lib/vncitizens_authentication.dart
@@ -1,3 +1,4 @@
 library vncitizens_authentication;
 
 export 'package:vncitizens_authentication/src/controllers/authentication_controller.dart';
+export 'package:vncitizens_authentication/src/models/authentication_status.dart';
